6b7c51bd17c2b837131adeb5f2f2ecb0411947ba
AgusT613 <torresagustin613@gmail.com>
2024-02-29 22:35:18 -0300

    Configure api-url constants for dev and production

--- services/urls.ts
+++ services/urls.ts
@@ -1,1 +1,4 @@
-export const ENVIRONMENT_ACTION_API = 'http://localhost:8000/api/environment-action/'
+const endpoint = 'api/environment-action/'
+const LOCAL_API_REST_URL = `http://localhost:8000/${endpoint}`
+const RENDER_API_REST_URL = `https://mysite-np2b.onrender.com/${endpoint}`
+export const ENVIRONMENT_ACTION_API = process.env.NODE_ENV === 'development' ? LOCAL_API_REST_URL : RENDER_API_REST_URL
