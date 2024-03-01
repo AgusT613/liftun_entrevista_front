8eef909e462931136ed5aa66106d63f3932a2775
AgusT613 <torresagustin613@gmail.com>
2024-02-29 23:39:50 -0300

    Delete constants. Add environment variables

--- services/urls.ts
+++ services/urls.ts
@@ -1,4 +1,1 @@
-const endpoint = 'api/environment-action/'
-const LOCAL_API_REST_URL = `http://localhost:8000/${endpoint}`
-const RENDER_API_REST_URL = `https://mysite-np2b.onrender.com/${endpoint}`
-export const ENVIRONMENT_ACTION_API = process.env.NODE_ENV === 'development' ? LOCAL_API_REST_URL : RENDER_API_REST_URL
+export const ENVIRONMENT_ACTION_API = `${process.env.NEXT_PUBLIC_ENVIRONMENT_ACTION_API}`
